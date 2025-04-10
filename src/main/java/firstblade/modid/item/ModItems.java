c454191aad12985a5237a2cee94ba3e09c09770a
RoseArchr <rosekelley@gmail.com>
2025-04-10 13:51:01 -0500

    added The First Blade item

--- src/main/java/firstblade/modid/item/ModItems.java
+++ src/main/java/firstblade/modid/item/ModItems.java
@@ -1,4 +1,33 @@
 package firstblade.modid.item;
 
+import firstblade.modid.FirstBlade;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+
+    public static final Item THE_FIRST_BLADE = registerItem("first_blade", new Item(
+            new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FirstBlade.MOD_ID,"first_blade")))));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(FirstBlade.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries ->{
+
+            entries.add(THE_FIRST_BLADE);
+
+        });
+
+
+    }
 }
